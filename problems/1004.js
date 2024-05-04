2b86b9be086c310b369f077ddd63cd11fdcd4889
llancruzz <alancruzsilva@gmail.com>
2024-05-04 22:14:02 +0100

    fix: Solution 1004 resolved.

---
+++ problems/1004.js
@@ -0,0 +1,10 @@
+"use strict";
+
+export function problem(lines) {
+  const valor1 = parseInt(lines[0]);
+  const valor2 = parseInt(lines[1]);
+
+  const PROD = valor1 * valor2;
+
+  console.log(`PROD = ${PROD}`);
+}
