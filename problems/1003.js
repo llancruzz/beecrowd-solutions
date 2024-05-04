a940da6d316d4c39a6c75770771f8d47fec0c3de
llancruzz <alancruzsilva@gmail.com>
2024-05-04 22:09:02 +0100

    fix: Solution 1003 resolved.

---
+++ problems/1003.js
@@ -0,0 +1,13 @@
+"use strict";
+
+export function problem(lines) {
+  // Lendo os valores de A e B
+  var A = parseInt(lines[0]);
+  var B = parseInt(lines[1]);
+
+  // Calculando a soma
+  var SOMA = A + B;
+
+  // Imprimindo o resultado formatado
+  console.log("SOMA = " + SOMA);
+}
